ddd4a2bb991cea450f9b56970aa4287be0e25057
Bishnu <bishnucit@gmail.com>
2013-07-02 01:30:18 +0530

    Reads co-ordinate of sprite from the Plist file-
    Here the co-ordinates x_c and y_c are read from the Property List file
    by using CCDictionary and then reading them as CCString and finally
    converting them toInt().

---
+++ Classes/HelloWorldScene.cpp
@@ -0,0 +1,123 @@
+#include "HelloWorldScene.h"
+
+using namespace cocos2d;
+
+CCScene* HelloWorld::scene()
+{
+    CCScene * scene = NULL;
+    do 
+    {
+        // 'scene' is an autorelease object
+        scene = CCScene::create();
+        CC_BREAK_IF(! scene);
+
+        // 'layer' is an autorelease object
+        HelloWorld *layer = HelloWorld::create();
+        CC_BREAK_IF(! layer);
+
+        // add layer as a child to scene
+        scene->addChild(layer);
+    } while (0);
+
+    // return the scene
+    return scene;
+}
+
+// on "init" you need to initialize your instance
+bool HelloWorld::init()
+{
+    bool bRet = false;
+    do 
+    {
+        //////////////////////////////////////////////////////////////////////////
+        // super init first
+        //////////////////////////////////////////////////////////////////////////
+
+        CC_BREAK_IF(! CCLayer::init());
+
+        //////////////////////////////////////////////////////////////////////////
+        // add your codes below...
+        //////////////////////////////////////////////////////////////////////////
+
+        // 1. Add a menu item with "X" image, which is clicked to quit the program.
+
+        // Create a "close" menu item with close icon, it's an auto release object.
+        CCMenuItemImage *pCloseItem = CCMenuItemImage::create(
+            "CloseNormal.png",
+            "CloseSelected.png",
+            this,
+            menu_selector(HelloWorld::menuCloseCallback));
+        CC_BREAK_IF(! pCloseItem);
+
+        // Place the menu item bottom-right conner.
+        pCloseItem->setPosition(ccp(CCDirector::sharedDirector()->getWinSize().width - 20, 20));
+
+        // Create a menu with the "close" menu item, it's an auto release object.
+        CCMenu* pMenu = CCMenu::create(pCloseItem, NULL);
+        pMenu->setPosition(CCPointZero);
+        CC_BREAK_IF(! pMenu);
+
+        // Add the menu to HelloWorld layer as a child layer.
+        this->addChild(pMenu, 1);
+
+        // 2. Add a label shows "Hello World".
+
+        // Create a label and initialize with string "Hello World".
+        CCLabelTTF* pLabel = CCLabelTTF::create("Extracting Coordinates from PLIST file", "Arial", 24);
+        CC_BREAK_IF(! pLabel);
+
+        // Get window size and place the label upper. 
+        CCSize size = CCDirector::sharedDirector()->getWinSize();
+        pLabel->setPosition(ccp(size.width / 2, size.height - 50));
+
+        // Add the label to HelloWorld layer as a child layer.
+        this->addChild(pLabel, 1);
+
+        // 3. Add add a splash screen, show the cocos2d splash image.
+        CCSprite* pSprite = CCSprite::create("bg.png");
+        CC_BREAK_IF(! pSprite);
+
+        // Place the sprite on the center of the screen
+        pSprite->setPosition(ccp(size.width/2, size.height/2));
+
+        // Add the sprite to HelloWorld layer as a child layer.
+        this->addChild(pSprite, 0);
+
+
+		//this is where the real code starts which took me 1 fucking week to understand as there are so many tutorials in the net that are deprecated.... fucking none works..... this is how i discovered something nice :D
+
+		
+	cocos2d::CCDictionary  *towerPositions = CCDictionary::createWithContentsOfFile("TowersPosition.plist");
+	CCSprite * towerBase = CCSprite::create("open_spot.png");
+	
+
+	//int x = (int)towerPositions->objectForKey("x_c");
+	//int y = (int)towerPositions->objectForKey("y_c");
+	
+ /*	CCInteger *pX = (CCInteger*)towerPositions->valueForKey("x_c")->getCString();
+	CCInteger *pY = (CCInteger*)towerPositions->valueForKey("y_c")->getCString();*/
+	CCString *pX = (CCString*)towerPositions->objectForKey("x_c");
+	CCString *pY = (CCString*)towerPositions->objectForKey("y_c");
+////	int x = (int)pX->getValue();
+////	int y = (int)pY->getValue();
+	int x = pX->intValue();
+	int y = pY->intValue();
+
+
+	
+	towerBase->setPosition(ccp(x,y));
+	this->addChild(towerBase);
+
+
+        bRet = true;
+    } while (0);
+
+    return bRet;
+}
+
+void HelloWorld::menuCloseCallback(CCObject* pSender)
+{
+    // "close" menu item clicked
+    CCDirector::sharedDirector()->end();
+}
+
